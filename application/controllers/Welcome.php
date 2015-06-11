e91bfdb8a66543146543038940df1f8c78305b8a
unknown <nrv.rox.u@gmail.com>
2015-06-11 21:11:10 +0530

    Done some major functions in database.

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -9,5 +9,13 @@ class Welcome extends CI_Controller {
 		$this->load->view('login');
         $this->load->model('Dblink');
         $this->Dblink->addUser('svvsvd','sdvdbsd','sfg',true);
+        //$this->Dblink->addBaner('UserNo','startdate','enddate','typeid','contact');
+        $this->Dblink->addTask('taskid','devid','appkey');
+        $this->Dblink->addfetchdata('banid','appkey');
+        //$this->Dblink->addtypetasklink('typeid','tasid');
+       // $this->Dblink->findUser('svvsvd','sfg');
+        //$this->Dblink->getfetchtime(0);
+        $this->Dblink->findBanner(true,1);
+      
 	}
 }
--- application/models/Dblink.php
+++ application/models/Dblink.php
@@ -14,7 +14,105 @@ public function addUser($UserName,$ContactNo,$pwd,$isuser){
 $this->db->insert('userdata',$data);
 }
 
+public function addBaner($UserNo,$startdate,$enddate,$typeid,$contact){
+        
+        $data = array(
+			'userno' =>$UserNo,
+			'startdate' => $startdate,
+            'enddate' =>$enddate,
+            'typeid' => $typeid,
+            'contactno' => $contact
+		);
+       $this->db->insert('banner',$data); 
+        
+    }
 
+public function addTask($taskid,$devid,$appkey){ //appdata
+        
+        $data = array(
+			'taskid' =>$taskid,
+			'devid' => $devid,
+            'appkey' =>$appkey
+		);
+       $this->db->insert('appdata',$data); 
+        
+    }
 
+public function addfetchdata($banid,$taskid){
+        
+        $data = array(
+			'banid' =>$banid,
+			'fetchescount' => 0,
+            'taskid' =>$taskid
+		);
+       $this->db->insert('fetchdata',$data); 
+        
+    }
+
+public function addtypetasklink($typeid,$tasid){
+        
+        $data = array(
+			'typeid' =>$typeid,
+			'tasid' => $tasid
+            
+		);
+       $this->db->insert('typetasklink',$data); 
+        
+    }
+
+public function inceasefetch($banid,$taskid,$count){
+        
+        
+        $data = array(
+			'fetchescount' => $count, 
+		);
+
+    $this->db->where('banid' ,$banid);
+    $this->db->where(  'taskid',$taskid);
+       $this->db->update('fetchdata',$data); 
+        
+    }
+
+public function findUser($UserName,$password){
+
+        $data = array(
+			'username' =>$UserName,
+            'pwd' => $password
+		);
+    $query = $this->db->get_where('userdata',$data, null,null);
+    //var_dump($query->result());
+    if($query->num_rows == 1){
+        return true;
+    }
+    else{
+        return false;
+    }
+    }
+
+public function findBanner($islimit,$time)
+{ //islimit define the time range
+    $jst_nw = new DateTime();//date("Y-m-d 0:0:0");
+    $jst_nw_str = $jst_nw->format('Y-m-d');
+    if ($islimit == null) {
+    $nextdate = date_add($jst_nw, date_interval_create_from_date_string("365 days"));
+        }
+    else{
+    $nextdate = date_add($jst_nw, date_interval_create_from_date_string("$time days"));
+        }
+    $nextdate=$nextdate->format('Y-m-d');
+    $query = $this->db->query("Select * from banner  where enddate <= $nextdate and enddate >= $jst_nw_str");
+    $res=$query->num_rows();
+    return $res;
+
+    }
+
+public function getfetchtime($bannid){
+    $query=$this->db->query("Select * from fetchdata where banid = '$bannid'");
+    $res=$query->result();
+    return $res[0]->fetchescount;
+    }
+
+//public function
 }
+
 ?>
